ad3cf0fce556d3a663f80afb7f4f613310ceab75
Robyn C. Maehl <rcmaehl@gmail.com>
2025-10-15 10:15:47 -0400

    Resolve #4

--- localonly.bat
+++ localonly.bat
@@ -14,10 +14,28 @@ set "pass=PASSWORDPH"
 echo.
 echo WARNING: Using the following characters will brick your install: /\[]:;^|=,+*?^<^>"
 echo WARNING: Using a username longer than 20 characters will brick your install
-echo.
-set /P newname=Please Enter Username to Use: || set newname=admin
-set /P newshow=Please Enter Display Name to Use: || set newshow=admin
-set /P newpass=Please Enter Password to Use: || set newpass=password
+echo. 
+:SetName
+set /P newname=Please Enter Username to Use: || set newname=
+if "%newname%"="" (
+    echo No Username was specified, defaulting to ADMIN
+    set newname=admin
+)
+:SetDisplay
+set /P newshow=Please Enter Display Name to Use: || set newshow=
+if "%newshow%"="" (
+    echo No DisplayName was specified, defaulting to %newname%
+    set newshow=%newname%
+)   
+:SetPass
+set /P newpass=Please Enter Password to Use: || set newpass=
+if "%newpass%"="" (
+    echo No Password was specified, this is unsafe.
+    echo Please Enter "IAmAbsolutelySureIWantABlankPassword" to set a Blank Password
+    goto SetPass
+)
+if "%newpass%"="IAmAbsolutelySureIWantABlankPassword" set newpass=
+
 for /F "delims=" %%a in (C:\Windows\Panther\autounattend.xml) DO (
    set line=%%a
    setlocal EnableDelayedExpansion
@@ -39,6 +57,7 @@ for /F "delims=" %%a in (C:\Windows\Panther\stage2.xml) DO (
 move /Y C:\Windows\Panther\stage3.xml C:\Windows\Panther\autounattend.xml >nul
 del /Q C:\Windows\Panther\stage1.xml
 del /Q C:\Windows\Panther\stage2.xml
+
 echo.
 echo Press Any Key to Reboot and Apply Changes
 pause >nul

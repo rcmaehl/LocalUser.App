838b67eb73d68a307c9fed1a636e99fa5cf86484
Robyn C. Maehl <rcmaehl@gmail.com>
2025-10-15 10:21:51 -0400

    Fix Syntax Errors

--- localonly.bat
+++ localonly.bat
@@ -17,24 +17,27 @@ echo WARNING: Using a username longer than 20 characters will brick your install
 echo. 
 :SetName
 set /P newname=Please Enter Username to Use: || set newname=
-if "%newname%"="" (
+if "%newname%"=="" (
     echo No Username was specified, defaulting to ADMIN
+    echo.
     set newname=admin
 )
 :SetDisplay
 set /P newshow=Please Enter Display Name to Use: || set newshow=
-if "%newshow%"="" (
+if "%newshow%"=="" (
     echo No DisplayName was specified, defaulting to %newname%
+    echo.
     set newshow=%newname%
 )   
 :SetPass
 set /P newpass=Please Enter Password to Use: || set newpass=
-if "%newpass%"="" (
+if "%newpass%"=="" (
     echo No Password was specified, this is unsafe.
     echo Please Enter "IAmAbsolutelySureIWantABlankPassword" to set a Blank Password
+    echo.
     goto SetPass
 )
-if "%newpass%"="IAmAbsolutelySureIWantABlankPassword" set newpass=
+if "%newpass%"=="IAmAbsolutelySureIWantABlankPassword" set newpass=
 
 for /F "delims=" %%a in (C:\Windows\Panther\autounattend.xml) DO (
    set line=%%a
